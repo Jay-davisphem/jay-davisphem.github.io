580d216afeeb2c8b1c2c01977aea0380acff4dd3
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-06 23:37:05 +0100

    done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import letsstud from "../../Assets/Projects/letsstudpng.png";
+import liru from "../../Assets/Projects/liru.png";
+import portfolio from "../../Assets/Projects/portfolio.png";
+import newsfeeder from "../../Assets/Projects/newsfeeder.png";
+import todo from "../../Assets/Projects/todo.png";
+import notty from "../../Assets/Projects/notty.png";
 
 function Projects() {
   return (
@@ -23,69 +23,74 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={newsfeeder}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Newsfeeder"
+              description="A web app that uses currentnewsapi to render data. It fetches the latest news from around the world. It is written with ReactJS, react-router-dom, news api, hooks."
+              ghLink="https://github.com/jay-davisphem/newsfeeder"
+              demoLink="https://newsfeeder.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={notty}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="React-notty"
+              description="A note-taking web application built for convenience. It showcases my skill in using react hooks and handling data efficiently. It is written with ReactJS, hooks and localStorage"
+              ghLink="https://github.com/jay-davisphem/react-notty"
+              demoLink="https://notty.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={portfolio}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              title="Portfolio website"
+              description="My portfolio website build to showcase my work, about me and links to github repos. Made use of reactjs, react-router-dom, bootstrap, react-pdf"
+              ghLink="https://github.com/jay-davisphem/jay-davisphem.github.io"
+              demoLink="https://davisphem.me/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={letsstud}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Letsstudy web app"
+              description="A study platform where students, study buddies, and friends can chat and study together. The app contains all the necessary CRUD operations and query manipulation and authentication. The API is built with Django rest framework and can be consumed by anyone. Made with Django, djangorestframework, postgresqsl, css. The api is located in /api routes"
+              ghLink="https://github.com/jay-davisphem/letsStudy"
+              demoLink="https://letsstud.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={liru}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Minimalistic Chat rooms"
+              description="A CRUD application written in django. A user can register, login create rooms and chat with any other connected members. All messages are cleared in after 30minutes of inactivity or when a user logoff. Made use of Django, HTML, CSS, Postgresql"
+              ghLink="https://github.com/jay-davisphem/lirurooms"
+              demoLink="https://lirurooms.herokuapp.com"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={todo}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+              title="Todo App Frontend(Reactjs) & Backend(Django)"
+              description="A version2 of my wannado todo app written with django and react. Written in Djangorestframework and Reactjs"
+              ghLink="https://github.com/jay-davisphem/wannado2"
+              demoLink="https://todo.wannado.social/"
             />
+          </Col>
+          <Col md={5} className='project-card'>
+            <ProjectCard
+              ghlink="https://github.com/Jay-davisphem?tab=repositories"
+              title="And lots more on github"
+              description=''/>
           </Col>
         </Row>
       </Container>
